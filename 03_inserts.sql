b0d317e1e1738b52d02f34e11db146b990a2da83
Davi Silva Soares <davisilvasoares1@gmail.com>
2025-11-05 09:16:41 -0300

    Add initial data inserts for usuarios and reproducoes

---
+++ 03_inserts.sql
@@ -0,0 +1,9 @@
+INSERT INTO usuarios (nome, pais, data_cadastro, plano)
+VALUES
+('Maria Souza', 'Brasil', '2024-03-15', 'Free'),
+('Pedro Santos', 'Portugal', '2024-11-20', 'Premium');
+
+INSERT INTO reproducoes (id_usuario, data_reproducao, duracao_segundos, categoria)
+VALUES
+(1, '2024-03-16', 3600, 'Filme'),
+(4, '2025-09-07', 4200, 'Infantil');
