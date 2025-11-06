5896e007517a293a33e21d67e4328e410bb8f049
Davi Silva Soares <davisilvasoares1@gmail.com>
2025-11-05 23:54:22 -0300

    Create 04_queries.sql

---
+++ streamnow-parcionamento/scripts/04_queries.sql
@@ -0,0 +1,12 @@
+select*from usuarios;
+select*from reproducoes;
+
+SELECT COUNT(*) AS total_usuarios
+FROM usuarios
+WHERE pais = 'Brasil';
+
+select tableoid::regclass AS particao, *
+from usuarios;
+
+select tableoid::regclass AS particao, *
+from reproducoes;
